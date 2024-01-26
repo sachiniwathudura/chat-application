24355490c8b3a9f3cc53aaf536259158d8e1b26f
sachiniwathudura <sachiniwathudura@gmail.com>
2024-01-26 22:55:18 +0530

    2nd commit

---
+++ Group-Chat/src/main/java/lk/ijse/AppInitializer.java
@@ -0,0 +1,16 @@
+package lk.ijse;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+    public static void main(String[] args) {
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+
+}
