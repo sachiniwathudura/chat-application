e9aa851aa4a0ab83a20d1736cc799c014b5c6605
sachini <sachiniwathudura@gmail.com>
2024-01-28 06:19:31 +0530

    navigation

--- Group-Chat/src/main/java/lk/ijse/AppInitializer.java
+++ Group-Chat/src/main/java/lk/ijse/AppInitializer.java
@@ -1,14 +1,24 @@
 package lk.ijse;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class AppInitializer extends Application {
     public static void main(String[] args) {
+        launch(args);
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage ) throws Exception {
+        primaryStage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/veiw/LoginForm.fxml"))));
+
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("join chat");
+        primaryStage.show();
+        primaryStage.centerOnScreen();
 
     }
 
--- Group-Chat/src/main/java/lk/ijse/controller/LoginFromController.java
+++ Group-Chat/src/main/java/lk/ijse/controller/LoginFromController.java
@@ -1,4 +1,45 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.IOException;
+
 public class LoginFromController {
+    @FXML
+     TextField txtJoin;
+
+    public void btnJoinOnAction(ActionEvent actionEvent) {
+
+        Stage stage = new Stage();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/veiw/ClientFromController.fxml"));
+
+        try {
+            stage.setScene(new Scene(fxmlLoader.load()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        stage.setResizable(false);
+        stage.setTitle(" in your chat");
+
+        stage.centerOnScreen();
+        stage.show();
+        stage.setOnCloseRequest(Event::consume);
+        //clearFields();
+
+    }
+  /*  void clearFields() {
+        txtJoin.setText("");
+
+    }
+
+   */
 }
