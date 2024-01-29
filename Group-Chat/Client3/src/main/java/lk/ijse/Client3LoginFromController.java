e6688b3e5c6f37e31d8f40232c804ae3f6d1df7b
sachini <sachiniwathudura@gmail.com>
2024-01-29 13:56:52 +0530

    client3 done

---
+++ Group-Chat/Client3/src/main/java/lk/ijse/Client3LoginFromController.java
@@ -0,0 +1,29 @@
+package lk.ijse;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Client3LoginFromController {
+    @FXML
+    private TextField txtUserName;
+
+    public static String user_name;
+
+    @FXML
+    void btnJoinOnAction(ActionEvent event) throws IOException {
+
+        user_name = txtUserName.getText();
+        txtUserName.clear();
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Client2ChatFrom.fxml"))));
+        stage.setTitle("Chat Room");
+        stage.show();
+    }
+}
---
+++ Group-Chat/Client3/src/main/java/lk/ijse/Client3Wrapper.java
@@ -0,0 +1,7 @@
+package lk.ijse;
+
+public class Client3Wrapper {
+    public static void main(String[] args) {
+        Client3Initializer.main(args);
+    }
+}
